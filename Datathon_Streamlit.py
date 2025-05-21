f2f5bb4be772f19503d5cdad9882d8aa95965480
Kaue Anselmo <kaueanselmo@MacBook-Pro---Kaue.local>
2025-05-22 00:06:20 +0200

    Update Datathon_Streamlit.py with JSON loading and chart logic

--- Datathon_Streamlit.py
+++ Datathon_Streamlit.py
@@ -112,14 +112,14 @@ df_Prospects.head()
 
 
 
-# st.set_page_config(
-#     page_title = 'PAINEL DE AÇÕES DA B3',
-#     layout = 'wide'
-# )
+st.set_page_config(
+    page_title = 'PAINEL DE AÇÕES DA B3',
+    layout = 'wide'
+)
 
-# st.header("**PAINEL DE PREÇO DE FECHAMENTO E DIVIDENDOS DE AÇÕES DA B3**")
+st.header("**PAINEL DE PREÇO DE FECHAMENTO E DIVIDENDOS DE AÇÕES DA B3**")
 
-# # st.markdown("**PAINEL DE PREÇO DE FECHAMENTO E DIVIDENDfffOS DE AÇÕES DA B3**")
+st.markdown("**PAINEL DE PREÇO DE FECHAMENTO E DIVIDENDfffOS DE AÇÕES DA B3**")
 
 
 # ticker = st.text_input('Digite o ticket da ação', 'BBAS3')
@@ -141,3 +141,4 @@ df_Prospects.head()
 
 # st.line_chart(tickerDF.Close)
 # st.bar_chart(tickerDF.Dividends)
+
